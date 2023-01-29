6eb4592e83b0229db5760106305e5c57ab81ae67
Aleksander <alex-vodon@mail.ru>
2023-01-29 10:10:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 6500;
+
+        int amout = 20;
+
+        int mile = price / amout;
+        
+        System.out.println("Количество милей:" + mile);
+
+    }
+}
